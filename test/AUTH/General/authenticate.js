afc6b36d104422c4ae50c863429bd30f8605fb11
Miruna Lupascu <mirunastefania2001@gmail.com>
2023-03-21 22:18:30 +0200

    Errors in authenticate

--- test/AUTH/General/authenticate.js
+++ test/AUTH/General/authenticate.js
@@ -17,3 +17,25 @@ describe('Login should be successful for valid credentials', async() => {
      await AuthApp.loginWithNewAccount();
    })
 });
+
+// describe('Login should be unsuccessful for invalid credentials', async() => {
+//    before('Navigate to login page for Moodle', async () => {
+//         await AuthApp.open();
+//         await AuthApp.successfulOpen();
+//         await AuthApp.navigateToLogin();
+//    }); 
+
+// //    it('for existing account', async () => {
+// //        await AuthApp.loginWithExistingAcc(); 
+// //    });
+
+//    it('for invalid email', async () => {
+//       await AuthApp.loginWithInvalidEmail();
+//    })
+
+//    // it('for invalid password', async() => {
+//    //    await AuthApp.loginWithInvalidPassword();
+//    // })
+
+// });
+
