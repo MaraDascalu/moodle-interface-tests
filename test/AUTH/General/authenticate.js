b83435247985b6f36d256d658c6abc740d41118e
Miruna Lupascu <mirunastefania2001@gmail.com>
2023-03-21 23:54:05 +0200

    Added test for invalid user

--- test/AUTH/General/authenticate.js
+++ test/AUTH/General/authenticate.js
@@ -3,6 +3,23 @@ import AuthAppActions from "../../../lib/util/AuthApp/actions.js";
 
 const AuthApp = new AuthAppActions();
 
+describe('Login should be unsuccessful for invalid credentials', async() => {
+   beforeEach('Navigate to login page for Moodle', async () => {
+        await AuthApp.open();
+        await AuthApp.successfulOpen();
+        await AuthApp.navigateToLogin();
+   }); 
+
+   it('for invalid password', async() => {
+      await AuthApp.loginWithInvalidPassword();
+   })
+
+   it('for invalid email', async () => {
+      await AuthApp.loginWithInvalidEmail();
+   })
+});
+
+
 describe('Login should be successful for valid credentials', async() => {
    before('Navigate to login page for Moodle', async () => {
         await AuthApp.open();
@@ -13,29 +30,8 @@ describe('Login should be successful for valid credentials', async() => {
 //    it('for existing account', async () => {
 //        await AuthApp.loginWithExistingAcc(); 
 //    });
+
    it('for new account', async () => {
-     await AuthApp.loginWithNewAccount();
+    await AuthApp.loginWithNewAccount();
    })
 });
-
-// describe('Login should be unsuccessful for invalid credentials', async() => {
-//    before('Navigate to login page for Moodle', async () => {
-//         await AuthApp.open();
-//         await AuthApp.successfulOpen();
-//         await AuthApp.navigateToLogin();
-//    }); 
-
-// //    it('for existing account', async () => {
-// //        await AuthApp.loginWithExistingAcc(); 
-// //    });
-
-//    it('for invalid email', async () => {
-//       await AuthApp.loginWithInvalidEmail();
-//    })
-
-//    // it('for invalid password', async() => {
-//    //    await AuthApp.loginWithInvalidPassword();
-//    // })
-
-// });
-
