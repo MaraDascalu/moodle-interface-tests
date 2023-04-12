ce35da7124c523cb89029ba5977abf79ab04d216
Miruna Lupascu <mirunastefania2001@gmail.com>
2023-04-12 11:45:48 +0300

    Added navigation tests

--- test/AUTH/Navigation/navigate.js
+++ test/AUTH/Navigation/navigate.js
@@ -1,5 +1,7 @@
 import { browser } from "@wdio/globals";
 import AuthAppActions from "../../../lib/util/AuthApp/actions.js";
+import { UriPath } from "../../../lib/util/AuthApp/constants.js";
+import { USER } from "../../../config/user.conf.js";
 
 const AuthApp = new AuthAppActions();
 
@@ -19,6 +21,28 @@ describe('Navigate between pages from auth', async () => {
         it('forward', async () => {
             await browser.back();
             await browser.forward();
+            await AuthApp.isOnSignInPage();
         });
+    });
+});
+
+describe('Navigate between set username email page and set password page', async () => {
+    beforeEach('Open sign in page', async () => {
+        await AuthApp.open();
+        await AuthApp.successfulOpen();
+        await AuthApp.navigateToLogin();
+        await AuthApp.setEmail(USER.VALID.EMAIL);
+        await browser.pause(3000);
+    });
+
+    it('back', async () => {
+        await browser.back();
+        await AuthApp.successfulOpen(UriPath.SIGN_IN);
+    });
+
+    it('forward', async () => {
+        await browser.back();
+        await browser.forward();
+        await AuthApp.isOnPasswordPage();
     });
 });
