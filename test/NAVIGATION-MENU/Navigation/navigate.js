287bdeb50b2e762b0dc5255774551e469eb6e84c
MaraDascalu <dascalu.mara.2001@gmail.com>
2023-04-22 12:31:16 +0300

    added navigation tests for nav bar menu options

--- test/NAVIGATION-MENU/Navigation/navigate.js
+++ test/NAVIGATION-MENU/Navigation/navigate.js
@@ -9,5 +9,43 @@ const NavBarApp = new NavBarAppActions();
 var loginFlag = false;
 
 describe('Navigate between nav bar menu options', () => {
-    //TODO: navigation 
+    beforeEach('Login user with vallid credentials', async () => {
+        await AuthApp.open();
+        await AuthApp.successfulOpen();
+        await AuthApp.navigateToLogin();
+        if (loginFlag == false) {
+            await AuthApp.loginWithNewAccount();
+            loginFlag = true;
+        }
+        await NavBarApp.successfulNavigateToWelcomePage();
+    });
+
+    Object.keys(NavBarOptions).forEach(option1 => {
+        describe(`${option1}`,  () => {
+            Object.keys(NavBarOptions).forEach(option2 => { 
+                describe(`${option2}`, () => {
+                    if (option1 !== option2) {
+                        beforeEach('Access two pages from nav bar menu', async () => {
+                            await NavBarApp.navigateToPageFromNavBar(option1);
+                            await NavBarApp.navigateToPageFromNavBar(option2);
+                        });
+
+                        it('navigate back', async () => {
+                            await NavBarApp.goBackToPreviosPage(option1);
+                        });
+
+                        it('navigate forward', async () => {
+                            await NavBarApp.goBackToPreviosPage(option1);
+                            await NavBarApp.goForwardToNextPage(option2);
+                        });
+                    }
+                });
+            });
+        });
+    })
+    
+    afterEach('Logout user', async () => {
+        await AuthApp.signOut();
+        await AuthApp.successfulOpen();
+    });
 });
